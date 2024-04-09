bc17b2861f57a2fae1e281c38025e09849e43166
alirezaamb <ababaei78@gmail.com>
2024-04-09 12:44:45 +0330

    fix

--- src/components/Button-Filter/ButtonFilter.js
+++ src/components/Button-Filter/ButtonFilter.js
@@ -6,7 +6,7 @@ export function ButtonFilter({ text }) {
     element: 'button',
     innerText: text,
     className:
-      'border-[2px] rounded-full font-semibold hover:bg-gray-700 hover:text-white border-black inline-block px-[20px] text-[16px] m-auto py-[5px]',
+      'border-[2px] rounded-full font-semibold hover:bg-gray-700 hover:text-white border-black inline-block px-6 text-4 m-auto py-[5px]',
   });
   // bg-white text-black px-4 rounded-3xl focus:bg-[#343A40] focus:text-white border-[#343A40] border-2 border-solid
 
