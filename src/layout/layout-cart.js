88d99d94088c9c00f6aa0871445053b8585a2b27
alirezaamb <ababaei78@gmail.com>
2024-04-14 22:00:01 +0330

    feat:footer of cart & set navigate

--- src/layout/layout-cart.js
+++ src/layout/layout-cart.js
@@ -1,4 +1,5 @@
 import { El } from '../utils/create-element';
+import { footerCart } from './footer/footer-cart';
 import { headerCart } from './header/header-Cart';
 
 export function layoutCart(main) {
@@ -8,6 +9,7 @@ export function layoutCart(main) {
     children: [
       headerCart(),
       El({ element: 'main', className: 'container mx-auto', children: [main] }),
+      footerCart(),
     ],
   });
 
